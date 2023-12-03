290f6d4fa0fd18e47e447460f15ca5dc24c43a49
Dima <dumitruwebdev@gmail.com>
2023-12-03 17:20:46 +0200

    master

--- routes/courses.js
+++ routes/courses.js
@@ -17,6 +17,7 @@ rounter.get('/:id', async (req, res) => {
     const course = await Course.getById(req.params.id);
 
     res.render('course', {
+        layout: 'empty',
         title: `Course ${course.title}`,
         course
     });
